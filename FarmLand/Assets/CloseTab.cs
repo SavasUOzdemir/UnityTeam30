a9bcebaad053fe952c8ac4a03255ba68c58e7794
SavasUOzdemir <savas.ozdemir@ozu.edu.tr>
2022-05-23 21:17:30 +0300

    Sidebar

---
+++ FarmLand/Assets/CloseTab.cs
@@ -0,0 +1,13 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CloseTab : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void onclick()
+//    {
+//        gameObject.GetComponentInParent.transform.LeanMoveLocalX(0, 0.5f).setEaseOutCubic();
+
+//    }
+//}
